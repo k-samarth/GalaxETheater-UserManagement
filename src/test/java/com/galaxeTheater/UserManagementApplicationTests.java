32f5e934dcc00001b51b9afdc19f3a051296dbb2
K Samarth N Kamath <kamathsamarth@gmail.com>
2022-12-02 11:25:54 +0530

    Update UserManagementApplicationTests.java

--- src/test/java/com/galaxeTheater/UserManagementApplicationTests.java
+++ src/test/java/com/galaxeTheater/UserManagementApplicationTests.java
@@ -1,14 +1,9 @@
 package com.galaxeTheater;
 
-import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.galaxeTheater.entities.Role;
-import com.galaxeTheater.entities.User;
-import com.galaxeTheater.service.UserService;
+
 
 @SpringBootTest
 class UserManagementApplicationTests {
