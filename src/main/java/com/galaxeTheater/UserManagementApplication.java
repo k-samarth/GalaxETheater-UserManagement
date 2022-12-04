7ef9c6ef5f82633da2a3ac41e28a8418f2543e07
K Samarth N Kamath <kamathsamarth@gmail.com>
2022-12-04 21:37:09 +0530

    Update UserManagementApplication.java

--- src/main/java/com/galaxeTheater/UserManagementApplication.java
+++ src/main/java/com/galaxeTheater/UserManagementApplication.java
@@ -5,10 +5,14 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
 
 import com.galaxeTheater.mapper.UserDTOMapper;
 import com.galaxeTheater.service.serviceImpl.UserServiceImpl;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class UserManagementApplication {
 	public static void main(String[] args) {
